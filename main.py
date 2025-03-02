f4ffbaeefdcc3e85676f7f036540f5ed85aa0431
sam-b02 <samarbaskar02@gmail.com>
2025-03-03 04:40:08 +0530

    add audio player util

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from utils import image_encoding
+from utils import audio_util
 
 mode = input("Do you want to (T)ransmit or (R)ecieve?")
 
 if mode == "T":
     image = image_encoding.encode_image(input(r"please enter image path"), True)
     print("image has been successfully encoded!")
+
+    audio_util.play_tone(1000, 1)
---
+++ utils/audio_util.py
@@ -0,0 +1,29 @@
+import pyaudio
+import numpy as np
+
+def play_tone(frequency=1000, duration=3, volume=0.5):
+
+    p = pyaudio.PyAudio()
+
+    sample_rate = 44100  # CD quality
+    samples = int(duration * sample_rate)
+
+    t = np.linspace(0, duration, samples, False)
+    tone = volume * np.sin(2 * np.pi * frequency * t)
+    
+    audio_data = tone.astype(np.float32).tobytes()
+
+    stream = p.open(
+        format=pyaudio.paFloat32,
+        channels=1,
+        rate=sample_rate,
+        output=True
+    )
+
+    stream.write(audio_data)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    
+    print(f"Played {frequency}Hz tone for {duration} seconds")
