d94cce72506b00612a6b2c29f9750a41a9396a75
sam-b02 <samarbaskar02@gmail.com>
2025-03-04 17:56:16 +0530

    use new transmission and reciever

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
+from protocol import reciever
+from protocol import transmitter
 from utils import image_encoding
+
 from utils import audio_util
 
-audio_util = audio_util.AudioManager(44100)
-mode = input("Do you want to (T)ransmit or (R)ecieve?")
-max_retries = 10
-counter = 0
 
-if mode == "T":
-    image = image_encoding.encode_image(input(r"please enter image path"), True)
-    print("image has been successfully encoded!")
-
-    print("sending out engagement signal...")
-    detected_signal = False
-
-    while not detected_signal and counter <= max_retries:
-        audio_util.play_tone(850, 0.5)
-        audio_util.play_tone(650, 0.5)
-        
-        print("Listening for receiver response...")
-        detected_signal = audio_util.listen_for_tones([750], duration=3)
-        counter += 1
-        
-        if detected_signal:
-            print("Detected receiver! Starting handshake...")
-            audio_util.play_tone(750, 0.5)
-            break
-        else:
-            print("No response detected. Trying again in 2 seconds...")
-            import time
-            time.sleep(2)
+def main():
+    audio_manager = audio_util.AudioManager(44100)
     
-elif mode == "R":
-    print("Listening for incoming transmission...")
-    detected_signal = False
+    mode = input("Do you want to (T)ransmit or (R)eceive? ").upper()
     
-    while not detected_signal:
-        detected_signal = audio_util.listen_for_tones([850, 650], duration=5)
-        
-        if detected_signal:
-            print("Engagement signal detected!")
-            print("Sending acknowledgment...")
-            audio_util.play_tone(750, 0.5)
-
-            time.sleep(0.25)
-            
-            ack_detected = audio_util.listen_for_tones([750], duration=3)
-            if ack_detected:
-                print("Handshake established!")
-            else:
-                print("Sender didn't acknowledge. Listening again...")
-                detected_signal = False
+    try:
+        if mode == "T":
+            image = image_encoding.encode_image(input("Please enter image path: "), True)
+            print("Image has been successfully encoded!")
+            transmitter.transmit(audio_manager, image)
+        elif mode == "R":
+            reciever.receive(audio_manager)
         else:
-            print("No transmission detected. Continuing to listen...")
+            print("Invalid mode. Please choose T or R.")
+    finally:
+        audio_manager.close()
+
+if __name__ == "__main__":
+    main()
